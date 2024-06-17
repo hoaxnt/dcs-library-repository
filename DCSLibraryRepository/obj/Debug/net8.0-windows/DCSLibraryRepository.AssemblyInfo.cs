17111cbfb50a01f07fc7a16649f63db33e7ae3c9
Hoaxnt <regacho.marcjulius@gmail.com>
2024-06-17 15:47:31 -0800

    system changes

--- DCSLibraryRepository/obj/Debug/net8.0-windows/DCSLibraryRepository.AssemblyInfo.cs
+++ DCSLibraryRepository/obj/Debug/net8.0-windows/DCSLibraryRepository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DCSLibraryRepository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c7e6bdd0718c3dbe1d7aa56ebc02a121a0212a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a7c8dcf3b3333c1289037531115235770fd8680")]
 [assembly: System.Reflection.AssemblyProductAttribute("DCSLibraryRepository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DCSLibraryRepository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
